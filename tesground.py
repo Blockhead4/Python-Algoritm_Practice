a2b0959ad8228abf3eb749ec1434da47d8d0718a
blockhead4 <blockhead4@naver.com>
2019-07-09 17:52:32 +0900

    add

--- tesground.py
+++ tesground.py
@@ -1,16 +1,18 @@
-def gcdlcm2(a, b):
-    if a < b:
-        a, b = b, a
-    if a % b != 0:
-        c = a % b
-        while c:
-            c , b = b % c, c   
-    return(b)
+# def gcdlcm2(a, b):
+#     if a < b:
+#         a, b = b, a
+#     if a % b != 0:
+#         c = a % b
+#         while c:
+#             c , b = b % c, c   
+#     return(b)
 
-
-]
     
-a=15
-b=24
+# a=15
+# b=24
 
-print(gcdlcm(a, b))
+# print(gcdlcm(a, b))
+
+s =  [[0 for i in range(4)] for i in range(5)]
+
+print(s)
